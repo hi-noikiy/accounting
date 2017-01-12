76f59e044ca6567bf029d081c67c8b884387d7c4
iugo <iugogogo@gmail.com>
2017-01-12 16:45:59 +0800

    初始化脚手架

---
+++ app.js
@@ -0,0 +1,41 @@
+const Koa = require('koa');
+const co = require('co');
+const bodyParser = require('koa-bodyparser');
+const compress = require('koa-compress');
+const zlib = require('zlib');
+const _ = require('koa-route');
+
+const api = require('./src/api');
+
+const app = new Koa();
+
+// 解析 req body
+app.use(bodyParser());
+
+// res body gzip 压缩
+app.use(compress({
+  threshold: 1024,
+  flush: zlib.Z_SYNC_FLUSH,
+}));
+
+// 修改 res headers
+app.use(co.wrap(function* (ctx, next) {
+  ctx.type = 'application/json; charset=utf-8';
+  yield next();
+}));
+
+// 404 处理
+app.use(co.wrap(function* (ctx, next) {
+  yield next();
+  if (ctx.body === undefined) {
+    ctx.body = '默认输出.';
+  }
+}));
+
+// api
+app.use(_.get('/api/out', api.out));
+
+const port = process.env.PORT || 9876;
+app.listen(port);
+
+console.log(`服务开始运行于: http://localhost:${port}`);
