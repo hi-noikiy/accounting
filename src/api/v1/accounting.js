179bb358a4e548ea9980d70c2b09cc07b486f0be
iugo <iugogogo@gmail.com>
2017-01-24 10:13:25 +0800

    整理财务模块文件结构

---
+++ src/api/v1/accounting.js
@@ -0,0 +1,41 @@
+const accounting = require('../../accounting');
+const ExpectedError = require('../error');
+
+module.exports = {
+  getTitlesList: ctx => accounting.titles.list().then(res => {
+    ctx.body = JSON.stringify(res.rows);
+  }),
+
+  newTitle: ctx => {
+    accounting.titles.create(ctx.request.body).then(res => {
+      console.log(res);
+    });
+    ctx.response.status = 201;
+    ctx.length = 0;
+  },
+
+  editTitle: (ctx, id) => {
+    console.log(`请求编辑 id 为 ${id} 的科目`);
+    const valid = id > 0;
+    if (!valid) {
+      throw new ExpectedError(1004);
+    }
+    ctx.response.status = 204;
+    ctx.length = 0;
+  },
+
+  deleteTitle: (ctx, id) => {
+    console.log(`请求删除 id 为 ${id} 的科目`);
+    const valid = id > 0;
+    if (!valid) {
+      throw new ExpectedError();
+    }
+    ctx.response.status = 204;
+    ctx.length = 0;
+  },
+
+  getDocumentsList: ctx => {
+    const vouchers = accounting.vouchers;
+    ctx.body = JSON.stringify(vouchers.list());
+  },
+};
--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -1,47 +1,19 @@
 const co = require('co');
-const accounting = require('../../accounting');
+
+const accounting = require('./accounting');
 const department = require('./department');
-const ExpectedError = require('../error');
 
-module.exports = Object.assign({
-  out: co.wrap(function* (ctx, next) {
-    const start = new Date();
-    yield next();
-    const ms = new Date() - start;
-    ctx.body = `${ctx.method} ${ctx.url} - ${ms}ms`;
-    console.log('正常输出');
-    return 'hehe';
-  }),
-
-  getTitlesList: ctx => accounting.titles.list().then(res => {
-    ctx.body = JSON.stringify(res.rows);
-  }),
-
-  newTitle: ctx => {
-    accounting.titles.create(ctx.request.body).then(res => {
-      console.log(res);
-    });
-    ctx.response.status = 201;
-    ctx.length = 0;
+module.exports = Object.assign(
+  {
+    out: co.wrap(function* (ctx, next) {
+      const start = new Date();
+      yield next();
+      const ms = new Date() - start;
+      ctx.body = `${ctx.method} ${ctx.url} - ${ms}ms`;
+      console.log('正常输出');
+      return 'hehe';
+    }),
   },
-
-  editTitle: (ctx, id) => {
-    console.log(`请求编辑 id 为 ${id} 的科目`);
-    const valid = id > 0;
-    if (!valid) {
-      throw new ExpectedError(1004);
-    }
-    ctx.response.status = 204;
-    ctx.length = 0;
-  },
-
-  deleteTitle: (ctx, id) => {
-    console.log(`请求删除 id 为 ${id} 的科目`);
-    const valid = id > 0;
-    if (!valid) {
-      throw new ExpectedError();
-    }
-    ctx.response.status = 204;
-    ctx.length = 0;
-  },
-}, department);
+  accounting,
+  department
+);
